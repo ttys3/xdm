c6deedd704e0099d12d783a8f2b8e52f76acd516
ttyS3 <ttys3.rust@gmail.com>
2023-07-17 12:44:39 +0800

    build: make-pkg.sh also generate crx and crx info json file

--- app/XDM/XDM.Linux.Installer/make-pkg.sh
+++ app/XDM/XDM.Linux.Installer/make-pkg.sh
@@ -12,3 +12,17 @@ rm -rf ./${OUT_DIR}
 mkdir -p ${OUT_DIR}/chrome-extension
 
 dotnet publish -c Release -f net6.0 -r linux-x64 --self-contained ../XDM.Gtk.UI/XDM.Gtk.UI.csproj -o ${OUT_DIR}
+
+echo "packing chrome extension ..."
+
+/usr/bin/microsoft-edge-stable --pack-extension=$(pwd)/${OUT_DIR}/chrome-extension
+
+echo "packing chrome extension done"
+
+echo "$(pwd)/${OUT_DIR}/chrome-extension.crx"
+
+command -v crx3-info || npm -g i crx3-utils
+
+crx3-info < "$(pwd)/${OUT_DIR}/chrome-extension.crx" > "$(pwd)/${OUT_DIR}/chrome-extension.json"
+
+echo "you may need add the extension id to /etc/opt/edge/policies/managed/xxx.json whitelist"
